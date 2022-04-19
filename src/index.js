b92a17a0096aafc28b3ffb06a653921c7f6bf60d
dwyfrequency <jackdwyer@google.com>
2022-04-19 16:44:35 -0400

    adding files for es

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import firebaseConfig from "../hidden/firebaseConfig";
+import "firebase/performance";
+
+firebase.initializeApp(firebaseConfig);
+const perf = firebase.performance();
+console.log(perf.dataCollectionEnabled);
+
+async function component() {
+  const element = document.createElement("div");
+  element.innerHTML = "hello";
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+// WITH NO CONFIG, -rw-r--r--  1 jackdwyer  primarygroup    43K Apr 19 11:39 dist/main.js
+// Run `yarn run build`
+// Run `ls -lh dist/main.js` to get the above
