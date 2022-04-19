fb87d6adc6408aa4117a6d5905e0f53509c80b5e
dwyfrequency <jackdwyer@google.com>
2022-04-19 17:30:29 -0400

    Update with trace code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ firebase.initializeApp(firebaseConfig);
 const perf = firebase.performance();
 console.log(perf.dataCollectionEnabled);
 
+const trace = perf.trace("CUSTOM_TRACE_NAME");
+trace.start();
+
 async function component() {
   const element = document.createElement("div");
   element.innerHTML = "hello";
@@ -13,8 +16,10 @@ async function component() {
   return element;
 }
 
+trace.stop();
 document.body.appendChild(component());
 
+// No change in KB size when adding TRACE
 // WITH NO CONFIG, -rw-r--r--  1 jackdwyer  primarygroup    43K Apr 19 11:39 dist/main.js
 // Run `yarn run build`
 // Run `ls -lh dist/main.js` to get the above
